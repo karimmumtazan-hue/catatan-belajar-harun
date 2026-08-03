f759fc6e8ed9c5ef134bea286270c2427898ae66
some one <karimmumtazan@gmail.com>
2026-08-03 21:23:45 +0700

    first commit

---
+++ gabut.sh
@@ -0,0 +1,35 @@
+C:\Users\NamaPengguna>hostname   REM Ini perintah yang diketik, 'hostname'
+NAMA-KOMPUTER-WINDOWS            REM Ini output dari perintah 'hostname'
+C:\Users\NamaPengguna>cd Desktop REM Perintah baru
+C:\Users\NamaPengguna\Desktop>_  REM Prompt berubah nunjukin direktori baru, siap lagi
+
+REM Contoh di CMD Windows
+C:\Users\NamaPengguna>cd
+C:\Users\NamaPengguna
+ 
+C:\Users\NamaPengguna>
+
+C:\ProyekWeb>dir
+ Volume in drive C is Windows
+ Volume Serial Number is XXXX-YYYY
+ 
+ Directory of C:\ProyekWeb
+ 
+07/17/2024  10:00 AM    <DIR>          .
+07/17/2024  10:00 AM    <DIR>          ..
+07/17/2024  10:00 AM             1,024 gambar.jpg
+07/17/2024  09:30 AM               512 index.html
+07/17/2024  09:45 AM               256 style.css
+               3 File(s)          1,792 bytes
+               2 Dir(s)  XYZ bytes free
+ 
+C:\ProyekWeb>
+
+C:\Users\NamaPengguna>cd Documents
+C:\Users\NamaPengguna\Documents>cd Arsip\2023
+C:\Users\NamaPengguna\Documents\Arsip\2023>cd ..\..
+C:\Users\NamaPengguna>
+
+REM Di CMD Windows
+md DataRahasia\Tahun2024\LaporanBulanan 
+REM (Biasanya otomatis bikin folder induknya juga)
